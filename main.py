0bc7edd53120f21b4e7e4b978a98ac3d0edd7dc7
Andrzej Skwierawski <skwierawski.andrzej@gmail.com>
2020-06-08 11:44:06 +0200

    Issue 11 : (Arguments in application)

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from FieldDrowing import *
 import numpy as np
 import os
 import neat
-
+import sys
+import argparse
 # # Creating new table of spots
 # Spots = np.empty(shape=(COLUMNS, ROWS), dtype=object)
 # OponentSpots = np.empty(shape=(COLUMNS, ROWS), dtype=object)
@@ -22,6 +23,9 @@ import neat
 # print_team_matrix(Spots)
 # print_oponent_team(OponentSpots)
 generation = 0
+number_of_generations = 10
+teamP = 'testTeam.xml'
+teamO = 'testTeamOP.xml'
 
 
 def eval_genoms(genomes, config):
@@ -35,21 +39,20 @@ def eval_genoms(genomes, config):
 
         # Creating new table of spots
         Spots = np.empty(shape=(COLUMNS, ROWS), dtype=object)
-        OponentSpots = np.empty(shape=(COLUMNS, ROWS), dtype=object)
+        OpponentSpots = np.empty(shape=(COLUMNS, ROWS), dtype=object)
         # making each spot free
         create_spots(Spots)
-        create_spots(OponentSpots)
+        create_spots(OpponentSpots)
         # importing team from xml
-        import_team_from_xml("testTeam.xml", Spots)
-        import_team_from_xml("testTeamOP2.xml", OponentSpots, True)
-
-        # print("Genome: ", genome_id)
-        # print_team_matrix(Spots)
-        # print_oponent_team(OponentSpots)
-        init(Spots, OponentSpots, genome, net, genome_of_generation, generation)
+        import_team_from_xml(teamP, Spots)
+        import_team_from_xml(teamO, OpponentSpots, True)
 
         # print_team_matrix(Spots)
-        # print_oponent_team(OponentSpots)
+        # print_oponent_team(OpponentSpots)
+        init(Spots, OpponentSpots, genome, net, genome_of_generation, generation)
+
+        # print_team_matrix(Spots)
+        # print_oponent_team(OpponentSpots)
         print("Genome", genome_id, "fitness:", genome.fitness, "\t1/fitness:", 1.0 / genome.fitness)
         genome.fitness = 1.0 / genome.fitness
 
@@ -64,12 +67,36 @@ def run(config_file):
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
 
-    winner = p.run(eval_genoms, 10)
+    winner = p.run(eval_genoms, number_of_generations)
 
     print('\nBest genome:\n{!s}'.format(winner))
 
 
+def parse_args():
+    print('Argument List:', str(sys.argv))
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--gens', '-g', type=int, default=10, help='Number of generations')
+    parser.add_argument('--teamp', '-p', type=str, default='testTeam.xml', help='File with player team')
+    parser.add_argument('--teamo', '-o', type=str, default='testTeamOP.xml', help='File with opponent team')
+
+    args = parser.parse_args()
+    if args.gens:
+        print("number of generations:", args.gens)
+        global number_of_generations
+        number_of_generations = args.gens
+    if args.teamp:
+        print("Player team:", args.teamp)
+        global teamP
+        teamP = args.teamp
+    if args.teamo:
+        print("Player team:", args.teamo)
+        global teamO
+        teamO = args.teamo
+
+
 if __name__ == '__main__':
+    parse_args()
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config-feedforward.txt')
     run(config_path)
